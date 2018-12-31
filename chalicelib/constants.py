c9dd1cecf4bc2477b146a28e337fcce62d13a23f
Vince Bloise <vbloise3@verizon.net>
2018-12-31 14:24:03 -0500

    Initial commit

---
+++ chalicelib/constants.py
@@ -0,0 +1,10 @@
+QUESTIONS_TABLE_NAME = 'trivia'
+QUESTIONS_TABLE_KEY_NAME = 'question_id'
+
+
+USERS_TABLE_NAME = 'trivia-users'
+USERS_TABLE_KEY_NAME = 'username'
+
+
+POOL_ARN = (
+    'arn:aws:cognito-idp:us-west-2:001178231653:userpool/us-west-2_vzTDHFYhq')
